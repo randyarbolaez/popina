822a33fee4904657390cbfbdf3c1c74ff5df6056
Randy <randyarbolaez0@gmail.com>
2019-12-06 23:04:31 -0500

    backend finished app

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,6 +4,7 @@ const Query = {
   restaurants: forwardTo("db"),
   restaurant: forwardTo("db"),
   restaurantsConnection: forwardTo("db"),
+  restaurantsConnection: forwardTo("db"),
   me(parent, args, ctx, info) {
     // check if current user ID
     if (!ctx.request.userId) {
